a3e4923a5a4281cd1137f1f31cb469d4e8b4e6ae
ruslav <mateusz.gajo9@wp.pl>
2021-11-28 13:35:00 +0100

    Create signin view

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signin from "features/views/Signin";
 
 const App: React.FunctionComponent = () => {
   return (
-    <div>
-      <p>dfsa</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </Router>
   );
 };
 
